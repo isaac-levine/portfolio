a7560600de778e2796f23861fe0a63296e235979
Isaac Levine <isaacmlevine4@gmail.com>
2024-12-20 13:31:43 -0500

    make sidebar button bigger

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,10 +38,10 @@ export const Sidebar = () => {
         )}
       </AnimatePresence>
       <button
-        className="fixed lg:hidden bottom-4 right-4 h-8 w-8 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center z-50"
+        className="fixed lg:hidden bottom-4 right-4 h-12 w-12 border border-neutral-200 rounded-full backdrop-blur-sm flex items-center justify-center z-50"
         onClick={() => setOpen(!open)}
       >
-        <IconLayoutSidebarRightCollapse className="h-4 w-4 text-secondary" />
+        <IconLayoutSidebarRightCollapse className="h-6 w-6 text-secondary" />
       </button>
     </>
   );
