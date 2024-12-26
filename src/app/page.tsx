30b21ddb433248335acb61d44b0f3c7f755a7835
Isaac Levine <isaacmlevine4@gmail.com>
2024-12-26 16:39:44 -0500

    hide tech stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         What I&apos;ve been working on
       </Heading>
       <Products />
-      <TechStack />
+      {/* <TechStack /> */}
     </Container>
   );
 }
